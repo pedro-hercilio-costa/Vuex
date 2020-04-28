d80f40dbe377e483954f11e32b250e521a66dbd6
Pedro Hercílio da Costa <pedro_hercilio-costa@estudante.sc.senai.br>
2020-04-28 18:01:53 -0300

    componenteVuex

---
+++ componentes/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">  
+    <caixas/>
+    <ferramentas @alteraCor="alteraCor($event)" @alterouTexto="alteraTexto($event)"/>
+  </div>
+</template>
+
+<script>
+import Caixas from "./components/Caixas.vue"
+import Ferramentas from "./components/Ferramentas.vue"
+export default {
+  name: 'App',
+  components: {
+    Caixas, Ferramentas
+  }, data: function () {
+    return {
+      
+    }    
+  }, methods: {
+    alteraCor: function(event) {
+      this.caixinhas[event.caixa].cor = event.cor      
+    }, alteraTexto: function(event) {
+      this.caixinhas[event.caixa].titulo = event.titulo   
+    }
+  }
+}
+</script>
+
+<style>
+</style>
